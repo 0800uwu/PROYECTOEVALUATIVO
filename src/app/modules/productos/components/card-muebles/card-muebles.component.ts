0a2ba36e68ea4984862a82f90e545fc6f5553ec6
0800uwu <joacosempaiii@gmail.com>
2024-09-25 18:16:36 -0300

    kelokeee profesoraa volvimos a trabajar despues de un arduo y cansador fin de semana de chamba (dormir), bueno, pudimos arreglar ciertos errores que no podiamos solucionar y al final resulto ser un div mal cerrado, continuando esto, en el proxima commit esperamos ya tener todo el trabajo terminado, diseño y cards arregladas. muchas gracias por leer un saludo a guido que lo quiero mucho y para agustin pepe novita tambien

--- src/app/modules/productos/components/card-muebles/card-muebles.component.ts
+++ src/app/modules/productos/components/card-muebles/card-muebles.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
 
 @Component({
   selector: 'app-card-muebles',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-muebles.component.css']
 })
 export class CardMueblesComponent {
+// Colección de todos los productos
+coleccionProductos: Producto[] = [];
 
+// Colección de sólo productos de categoría "Mesas"
+coleccionMuebles: Producto[] = [];
+
+productoSeleccionado!: Producto;
+
+modalVisible: boolean = false;
+
+constructor(public servicioCrud: CrudService){}
+
+ngOnInit(): void{
+  this.servicioCrud.obtenerProducto().subscribe(producto => {
+    this.coleccionProductos = producto;
+
+    // mostrar la colección actual de mesas
+    this.mostrarProductoMuebles();
+  })
+}
+
+// Función para filtrar los productos que sean del tipo "mesas"
+mostrarProductoMuebles(){
+  // forEach: itera la colección
+  this.coleccionProductos.forEach(producto => {
+    // Si la categoría del producto es igual a "juguetes", se enviará a la 
+    // colección de juguetes específicada
+
+    if(producto.categoria === "apartado3"){
+      // .push: sube o agrega un item a una colección
+      this.coleccionMuebles.push(producto);
+    }
+  })
+}
+
+// Muestra información completa de un producto elegido por el usuario
+mostrarVer(info: Producto){
+  this.modalVisible = true;
+
+  this.productoSeleccionado = info;
+}
 }
